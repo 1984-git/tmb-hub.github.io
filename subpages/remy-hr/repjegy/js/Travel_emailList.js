336e62dd3f6c3ae2f7df7b3e014630a1ad4810c4
John Doe <https://www.dreamuhome.com/>
2020-07-23 14:49:47 +0200

    Moving forward with the Travel Form first page

---
+++ subpages/remy-hr/repjegy/js/Travel_emailList.js
@@ -0,0 +1,65 @@
+function calculateName(emailAddress) {
+  let firstName = "";
+  let lastName = "";
+  
+  if (emailAddress.indexOf("@remygroup.com") > -1) {
+    emailAddress = emailAddress.substring(0, emailAddress.indexOf("@"));
+
+    firstName = emailAddress.substring(0, emailAddress.indexOf("."));
+    firstName = firstName.charAt(0).toUpperCase() + firstName.slice(1);
+    lastName = emailAddress.substring(emailAddress.indexOf(".") + 1);
+    lastName = lastName.charAt(0).toUpperCase() + lastName.slice(1);
+    return (firstName + " " + lastName);
+  }
+  else {
+    // If the email is not from REMYGROUP then just return the email address
+    firstName = emailAddress;
+    return (firstName);
+  }
+}
+
+function removeEmail(buttonReference) {
+  
+  let name = calculateName(buttonReference.innerHTML);
+  let pn = document.getElementById("passengerNames");
+
+  pn.value = pn.value.replace(name,'');
+  pn.value = pn.value.replace(", , ",", ");
+
+  // If now the pn starts with a ", " then lets remove it
+  if (pn.value.substring(0,2) == ", "){
+    pn.value = pn.value.slice(2);
+  }
+  
+  selectedEmails = document.getElementById("currentEmailList");
+  selectedEmails.removeChild(buttonReference);
+}
+
+function addNewEmail(inputField) {
+  // Create a new button and add it to the emaillist
+  var emailAddress = document.getElementById("passengerEmails");
+  newButton = document.createElement('button');
+  newButton.innerHTML = emailAddress.value;
+  newButton.id = emailAddress.value;
+  newButton.className = "btn btn-info emailBtn";
+
+  // Here a new we create the onclick event for the new button which simply removes it
+  newButton.onclick = function () {
+    removeEmail(this);
+  }
+
+  selectedEmails = document.getElementById("currentEmailList");
+  selectedEmails.appendChild(newButton);
+
+  // If there is already something in the box then add a ", " separator
+  let pn = document.getElementById("passengerNames");
+  if (pn.value != 0) {
+    pn.value += ", ";
+  }
+
+  pn.value += calculateName(emailAddress.value);
+
+  // Delete the content of the dropdown
+  emailAddress.value = "";
+  
+}
---
+++ subpages/remy-hr/repjegy/js/Travel_initForm.js
@@ -0,0 +1,71 @@
+
+function initFields(){
+
+    //Fill up the outbound flight enum
+    item = document.getElementById("outboundDestinationList");
+	optionArray = ["Düsseldorf - Budapest", "Frankfurt - Budapest", "Dortmund - Budapest", "Stuttgart - Budapest", "Bécs - Birmingham", 
+	                 "Bécs - Brüsszel", "Bécs - Tunisz", "Budapest - Birmingham", "Budapest - Brüsszel", "Budapest - Tunisz"];
+   
+    for (currentOption of optionArray) {
+        let optionItem = document.createElement('option'); // Listaelem létrehozása...
+        optionItem.innerHTML = currentOption;
+        item.appendChild(optionItem);
+    }            
+
+
+    //Fill up the return flight enum
+    item = document.getElementById("inboundDestinationList");
+	optionArray = ["Budapest - Düsseldorf", "Budapest - Frankfurt", "Budapest - Dortmund", "Budapest - Stuttgart", "Birmingham - Bécs",
+                    "Birmingham - Budapest", "Brüsszel - Bécs", "Brüsszel - Budapest", "Tunisz - Bécs", "Tunisz - Budapest"];
+   
+    for (currentOption of optionArray) {
+        let optionItem = document.createElement('option'); // Listaelem létrehozása...
+        optionItem.innerHTML = currentOption;
+        item.appendChild(optionItem);
+    }   
+
+ 
+    //Fill up the part of day enums (both)
+    items = [document.getElementById("outboundTime"), document.getElementById("inboundTime")];
+ 	optionArray = ["nincs", "reggel", "délelőtt", "délután", "este"];
+
+    for (item of items) {
+        for (currentOption of optionArray) {
+            let optionItem = document.createElement('option'); // Listaelem létrehozása...
+            optionItem.innerHTML = currentOption;
+            item.appendChild(optionItem);
+        }
+    }
+
+    //Fill up the flight type enum
+    //document.getElementById("inboundFlightType").style.disabled = true;
+
+    document.getElementById("inboundFlightType").disabled = true;;
+
+    items = [document.getElementById("outboundFlightType"), document.getElementById("inboundFlightType")];
+    optionArray = ["Normál (nem fapados)", "Fapados"];
+
+    for (item of items) {
+        for (currentOption of optionArray) {
+            let optionItem = document.createElement('option'); // Listaelem létrehozása...
+            optionItem.innerHTML = currentOption;
+            item.appendChild(optionItem);
+        }
+    }
+
+
+    //Fill up the passengers enum
+    item = document.getElementById("emailList");
+    optionArray = ["agnes.csontos@remygroup.com", "dirk.vanrompaey@remygroup.com", "mark.rahman@remygroup.com", 
+                    "martin.klein@remygroup.com", "tamas.orosz@remygroup.com", "janos.szavo@remygroup.com",
+                    "zoltan.erdei@remygroup.com", "kris.mattheus@remygroup.com", "zsuzsa.szokoly@remygroup.com",
+                    "peter.hansen@remygroup.com", "sascha.wiebers@remygroup.com", "krisztian.bacso@remygroup.com", 
+                    "fredrik.markholt@remygroup.com"];
+
+    for (currentOption of optionArray.sort()) {
+        let optionItem = document.createElement('option'); // Listaelem létrehozása...
+        optionItem.innerHTML = currentOption;
+        item.appendChild(optionItem);
+    }
+
+}
