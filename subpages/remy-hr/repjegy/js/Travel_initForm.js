a18507bf5e25283bf7b3a1084c180f72c0e80bd9
John Doe <https://www.dreamuhome.com/>
2020-07-23 15:30:27 +0200

    Moving forward. Travelform looks good now. No emails yet.

--- subpages/remy-hr/repjegy/js/Travel_initForm.js
+++ subpages/remy-hr/repjegy/js/Travel_initForm.js
@@ -1,6 +1,19 @@
 
 function initFields(){
 
+    item = document.getElementById("emailList");
+    optionArray = ["agnes.csontos@remygroup.com", "dirk.vanrompaey@remygroup.com", "fredrik.markholt@remygroup.com",
+                    "janos.szavo@remygroup.com", "kris.mattheus@remygroup.com", "krisztian.bacso@remygroup.com",
+                    "mark.rahman@remygroup.com", "martin.klein@remygroup.com", "peter.hansen@remygroup.com",
+                    "sascha.wiebers@remygroup.com", "tamas.orosz@remygroup.com", "zoltan.erdei@remygroup.com",
+                    "zsuzsa.szokoly@remygroup.com"]
+
+    for (currentOption of optionArray) {
+        let optionItem = document.createElement('option'); // Listaelem létrehozása...
+        optionItem.innerHTML = currentOption;
+        item.appendChild(optionItem);
+    } 
+    
     //Fill up the outbound flight enum
     item = document.getElementById("outboundDestinationList");
 	optionArray = ["Düsseldorf - Budapest", "Frankfurt - Budapest", "Dortmund - Budapest", "Stuttgart - Budapest", "Bécs - Birmingham", 
